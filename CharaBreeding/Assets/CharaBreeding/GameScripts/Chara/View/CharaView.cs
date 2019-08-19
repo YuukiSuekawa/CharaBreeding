a9d890c4cc522d7172bc495d3b724f2d934628d2
yuuki suekawa <suekawa@crestive-innovstion.co.jp>
2019-08-19 11:35:50 +0900

    ごはんアニメーション調整 嫌がり実装

--- CharaBreeding/Assets/CharaBreeding/GameScripts/Chara/View/CharaView.cs
+++ CharaBreeding/Assets/CharaBreeding/GameScripts/Chara/View/CharaView.cs
@@ -5,8 +5,12 @@ using UnityEngine.Events;
 
 public class CharaView : MonoBehaviour
 {
+    [SerializeField] private GameObject m_FoodRoot;
+    [SerializeField] private GameObject m_FoodPrefab;
+    
     static int AnimatorWalk = Animator.StringToHash("Walk");
     static int AnimatorAttack = Animator.StringToHash("Attack");
+
     Animator m_animator;
     private Vector3 initPos;
     private GameObject charaObject;
@@ -26,8 +30,6 @@ public class CharaView : MonoBehaviour
     {
         Vector3 startPos = charaObject.transform.localPosition;
         RectTransform trans = gameObject.GetComponent<RectTransform>();
-
-        // TODO 移動方向に向いておきたい
 
         int moveX = 10;
         int moveCountMax = 10;
@@ -80,15 +82,50 @@ public class CharaView : MonoBehaviour
     
     public IEnumerator AnimFood(UnityAction _callback)
     {
+        
+        int AnimatorFood01 = Animator.StringToHash("Eat01");
+        int AnimatorFood02 = Animator.StringToHash("Eat02");
+        int AnimatorFood03 = Animator.StringToHash("Eat03");
+
+        GameObject foodObj = Instantiate(m_FoodPrefab);
+        foodObj.transform.SetParent(m_FoodRoot.transform);
+        foodObj.transform.localPosition = Vector3.zero;
+
+        Animator foodAnimator = foodObj.GetComponent<Animator>();
+        
         nowAnimFlg = true;
         charaObject.transform.localPosition = initPos;
         charaObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+        foodAnimator.SetTrigger(AnimatorFood01);
         m_animator.SetTrigger(AnimatorAttack);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.5f);
+        foodAnimator.SetTrigger(AnimatorFood02);
         m_animator.SetTrigger(AnimatorAttack);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.5f);
+        foodAnimator.SetTrigger(AnimatorFood03);
         m_animator.SetTrigger(AnimatorAttack);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(1.5f);
+        Destroy(foodObj);
+        nowAnimFlg = false;
+        _callback();
+    }
+
+    /***
+     * 嫌がり
+     */
+    public IEnumerator AnimDecline(UnityAction _callback)
+    {
+        nowAnimFlg = true;
+        float time = 0.5f;
+        int rotateCount = 3;
+        charaObject.transform.localPosition = initPos;
+        for (int i = 0; i < rotateCount; i++)
+        {
+            charaObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+            yield return new WaitForSeconds(time);
+            charaObject.transform.rotation = Quaternion.Euler(0, 180, 0);
+            yield return new WaitForSeconds(time);            
+        }
         nowAnimFlg = false;
         _callback();
     }
