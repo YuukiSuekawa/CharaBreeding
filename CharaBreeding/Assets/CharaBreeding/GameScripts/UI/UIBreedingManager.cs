97ff96f67e0f57e788334bbaee5b5318c2a76357
yuuki suekawa <suekawa@crestive-innovstion.co.jp>
2019-08-28 14:51:51 +0900

    未実装ボタンの非アクティブ化

--- CharaBreeding/Assets/CharaBreeding/GameScripts/UI/UIBreedingManager.cs
+++ CharaBreeding/Assets/CharaBreeding/GameScripts/UI/UIBreedingManager.cs
@@ -1,4 +1,5 @@
-﻿using CharaBreeding.Util;
+﻿using Boo.Lang;
+using CharaBreeding.Util;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,6 +9,18 @@ namespace CharaBreeding.GameScripts.UI
     {
         [SerializeField] private MenuButton[] m_buttons;
 
+        private readonly List<MenuButton.ButtonType> m_unImplementedButtons = new List<MenuButton.ButtonType>
+        {
+            MenuButton.ButtonType.roomChange,
+            MenuButton.ButtonType.call,
+            MenuButton.ButtonType.discipline,
+            MenuButton.ButtonType.sleep,
+        };
+
+        private void Start()
+        {
+            LockUnImplemented();
+        }
 
         private MenuButton SearchMenuButton(MenuButton.ButtonType _type)
         {
@@ -17,6 +30,16 @@ namespace CharaBreeding.GameScripts.UI
             }
 
             return null;
+        }
+
+        // todo 全実装後、消します
+        private void LockUnImplemented()
+        {
+            foreach (var type in m_unImplementedButtons)
+            {
+                MenuButton button = SearchMenuButton(type);
+                button.interactable = false;
+            }
         }
 
         public void SetFoodEvent(UnityAction _callback)
